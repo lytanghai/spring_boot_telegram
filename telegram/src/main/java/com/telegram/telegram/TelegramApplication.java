599dde6e18ee6f03b40c04ae9d24cd76884ff05d
unknown <lytanghaii@gmail.com>
2024-08-16 00:07:55 +0700

    add log

--- telegram/src/main/java/com/telegram/telegram/TelegramApplication.java
+++ telegram/src/main/java/com/telegram/telegram/TelegramApplication.java
@@ -1,13 +1,20 @@
 package com.telegram.telegram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TelegramApplication.class);
+
 	public static void main(String[] args) {
+		log.info("uri 1: [{}]: req: url, content", "/telegram/group");
+		log.info("uri 2: [{}]: req: url, content", "/telegram/channel");
 		SpringApplication.run(TelegramApplication.class, args);
+
 	}
 
 }
